38c7360fd9f20fe85c3e5c89b222ce384f33486c
punpikorn.r <sadanthem@gmail.com>
2023-08-28 17:38:14 +0700

    fix frame + working on lg layout

--- src/FlavorPage.tsx
+++ src/FlavorPage.tsx
@@ -1,5 +1,7 @@
 import { useLocation, useParams } from "react-router-dom";
 import { FlavorConfigMap, getFlavorVariationConfig } from "./Flavors";
+import { useSyncExternalStore } from "react";
+import { useWindowDimensions } from "./useWindowDimensions";
 
 const getType = (search: string): string => {
   const query = new URLSearchParams(search);
@@ -9,6 +11,9 @@ const getType = (search: string): string => {
 export default function FlavorPage() {
   const params = useParams();
   const { search } = useLocation();
+  // const { width, height } = useWindowDimensions();
+
+  // console.log("width:", width);
 
   const flavor = params["flavor"] || "";
   const type = getType(search);
@@ -22,27 +27,50 @@ export default function FlavorPage() {
     // Default text color: text-slate-50
     <div
       className={
-        "flex flex-col md:flex-row md:h-full pb-6 font-serif text-slate-50 " +
+        "flex flex-col lg:h-[100vh] lg:flex-row font-serif text-slate-50 " +
         config.colorClass.background
       }
     >
-      <div className="flex flex-col md:basis-1/2">
-        <div className="absolute border-2 border-[#000001] inset-x-3 inset-y-3 h-72 md:w-[47.75%]"></div>
+      <div id="header-container" className="flex flex-col lg:basis-1/2">
+        {/* Dynamic height, corresponding to image size/ratio */}
+        <div
+          id="artwork-frame"
+          className="artwork-frame-sm-md absolute border-2 border-[#000001] inset-x-3 inset-y-3 lg:artwork-frame-lg"
+        ></div>
         <img
+          id="artwork-image"
           src={
             window.location.origin + "/flavorpedia/" + config.coverImageSource
           }
           alt="artwork"
         />
         <div
-          className={"bg-[#ffffff] md:grow px-6 pt-2 pb-8 " + config.colorClass.header}
+          id="header"
+          className={
+            "bg-[#ffffff] lg:grow px-6 lg:px-[4rem] pt-2 lg:pt-[4rem] pb-8 " +
+            config.colorClass.header
+          }
         >
           <p className="text-2xl font-bold">{config.formalName}</p>
           <p className="text-lg">{config.name}</p>
+          <div
+            className={
+              "hidden lg:block italic leading-7 lg:mt-[4rem] " +
+              config.colorClass.quote
+            }
+          >
+            <p className="text-base">&ldquo;{config.quote}&rdquo;</p>
+            {config.quoteBy ? <p>&mdash;&nbsp;{config.quoteBy}</p> : null}
+          </div>
         </div>
       </div>
-      <div className="px-3 pt-4 md:basis-1/2">
-        <div className={"italic leading-7 mb-4 " + config.colorClass.quote}>
+      <div className="px-3 pt-4 mb-6 lg:basis-1/2">
+        <div
+          id="quote-container"
+          className={
+            "lg:hidden italic leading-7 mb-4 " + config.colorClass.quote
+          }
+        >
           <p className="text-base">&ldquo;{config.quote}&rdquo;</p>
           {config.quoteBy ? <p>&mdash;&nbsp;{config.quoteBy}</p> : null}
         </div>
